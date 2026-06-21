d8decce9ad63cc4bf71eae4169c56be6960e51ea
i3mr9sxb <takuto.tsdwork@gmail.com>
2026-06-21 13:49:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ import fs from "fs";
 
 const DISCORD_ID = process.env.DISCORD_ID;
 
-const d = await api('POST', '/api/auth/send-code', { discordId: DISCORD_ID });
+async function api(method, path, body) {
+  const opts = { method, headers: { 'Content-Type': 'application/json' } };
+  if (body) opts.body = JSON.stringify(body);
+  const res = await fetch(path, opts);
+  return res.json();
+}
+
+const d = await api('POST', 'https://oogiri-bot-cfy1.onrender.com/api/auth/send-code', { discordId: DISCORD_ID });
 fs.writeFileSync("data/log.txt", JSON.stringify(d) + "\n")
