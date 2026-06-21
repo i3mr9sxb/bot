16507e9cbd60c50b5076a78d3f5a543db2d4268e
i3mr9sxb <takuto.tsdwork@gmail.com>
2026-06-21 13:54:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ import fs from "fs";
 const DISCORD_ID = process.env.DISCORD_ID;
 
 async function api(method, path, body) {
-  const opts = { method, headers: { 'Content-Type': 'application/json' } };
+  const opts = { 
+    method, 
+    headers: { 
+      'Content-Type': 'application/json',
+      "Referer": "https://oogiri-bot-cfy1.onrender.com/",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0",
+    }
+  };
   if (body) opts.body = JSON.stringify(body);
   const res = await fetch(path, opts);
   return res.json();
